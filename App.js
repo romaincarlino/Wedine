1a6db7db19bc9b17993e0373e9e517a638c9cfc3
romaincarlino <romain.carlino@gmail.com>
2018-05-12 15:12:21 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View>
+        <Text>
+          Welcome to React Native!
+        </Text>
+
+      </View>
+    );
+  }
+}
+
+const styles = {
+}
